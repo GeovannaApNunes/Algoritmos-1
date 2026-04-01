25a9bc379e38dce3d5ffee87aaa2e8153026519e
Geovanna Nunes <geovannanunes349@gmail.com>
2026-03-31 21:58:15 -0300

    31/03/26

---
+++ switch/switch.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+	
+	int mes;
+	
+	printf("Digite o mes: ");
+	scanf("%i", &mes);
+	
+	switch(mes){
+		case  1:
+			printf("Janeiro");
+			break;
+			
+		case 2:
+			printf("Fevereiro");
+			break;
+			
+		case 3:
+			printf("Marco");
+			break;
+			
+		case 4:
+			printf("Abril");
+			break;
+			
+		case 5:
+			printf("Maio");
+			break;
+			
+		defaut:
+			printf("Invalido");
+	}
+	
+	
+	
+}
