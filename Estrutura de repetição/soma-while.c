248ea443693ab4f958d620ec63948d3588f4d1cc
Geovanna Nunes <geovannanunes349@gmail.com>
2026-05-19 21:12:30 -0300

    Create soma-while.c

---
+++ Estrutura de repetição/soma-while.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	int i=1;
+	float nota, media, soma=0;
+	
+	while(i<= 5){
+		printf("Digite a %i nota: ", i);
+		scanf("%f", &nota);
+		
+		soma = nota+soma;
+		
+		i++;
+	}
+    
+    media = soma/5;
+    
+    printf("A media das notas e %.2f", media);
+    
+    return 0;
+}
