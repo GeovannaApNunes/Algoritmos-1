de72b38cde5d0a903202f598a76c483a2b966875
Geovanna Nunes <geovannanunes349@gmail.com>
2026-05-19 21:30:23 -0300

    Create funcionarios-dowhile.c

---
+++ Estrutura de repetição/funcionarios-dowhile.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int i=1, maior=0 , pergunta=1;
+    float salario, total=0;
+    
+    do{
+      printf("Quantidade do salario do %i funcionario: ", i);
+      scanf("%f", &salario);  
+      
+      if(salario > 1600){
+          maior= maior+1;
+      }
+      
+      printf("Deseja continuar prezado usuario?  ()1-sim 2-nao)");
+      scanf("%i", &pergunta);
+      i++;
+    }while(pergunta == 1);
+    
+    printf("A quantidade de funcionarios que ganham mais de R$1600 sao %i", maior);
+}
