5060667e24a18eece9d8076e38b9228dfe68016c
Geovanna Nunes <geovannanunes349@gmail.com>
2026-05-20 22:07:31 -0300

    Update 001.c

--- atividades a enviar/001.c
+++ atividades a enviar/001.c
@@ -11,7 +11,7 @@ int main()
       printf("Qual a idade da %i pessoa: ", i);
       scanf("%i", &idade);  
       
-      if(idade > 18){
+      if(idade >= 18){
           maior++;
       }
       i++;
